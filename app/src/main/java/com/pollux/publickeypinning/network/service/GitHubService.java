522e01b8fe61bcb6e55d7691f53179714714f067
sreekumar.av <sreekumar.av@ymedialabs.com>
2016-08-07 22:26:49 +0530

    Force enabled TLS v.1.2 support in 16+ above

--- app/src/main/java/com/pollux/publickeypinning/network/service/GitHubService.java
+++ app/src/main/java/com/pollux/publickeypinning/network/service/GitHubService.java
@@ -1,9 +1,17 @@
 package com.pollux.publickeypinning.network.service;
 
+import android.util.Log;
+
 import com.pollux.publickeypinning.network.api.GitHubApi;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+
 import okhttp3.CertificatePinner;
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
@@ -14,7 +22,7 @@ import retrofit2.Retrofit;
  * Copyright (C) 2016
  */
 public class GitHubService {
-
+    private static final String TAG = "GitHubService";
     public static final String HOST = "api.github.com";
     public static final String PUBLIC_KEY_HASH = "sha256/6wJsqVDF8K19zxfLxV5DGRneLyzso9adVdUN/exDacw=";
     public static final String END_POINT = "https://api.github.com/";
@@ -29,24 +37,65 @@ public class GitHubService {
 
         if (sService == null) {
 
-            CertificatePinner certificatePinner = new CertificatePinner.Builder()
-                    .add(HOST, PUBLIC_KEY_HASH)
-                    .build();
+            OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder();
+
+            /**
+             *
+             * Enable the Network Logs
+             *
+             */
 
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
 
-            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                    .certificatePinner(certificatePinner)
+            /**
+             * Set the SHA256 hash obtained from the Certificate
+             *
+             * Run this command to get the SHA256 fingerprint
+             * openssl s_client -connect api.github.com:443 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
+             *
+             *
+             */
+            CertificatePinner certificatePinner = new CertificatePinner.Builder()
+                    .add(HOST, PUBLIC_KEY_HASH)
+                    .build();
+
+
+            /**
+             * Force the connection to use only TLS v.1.2 avoiding the fallback to older version to avoid vulnerabilities
+             *
+             */
+            final ConnectionSpec.Builder connectionSpec =
+                    new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS);
+            connectionSpec.tlsVersions(TlsVersion.TLS_1_2).build();
+
+            /**
+             * Enable TLS specific version V.1.2
+             * Issue Details : https://github.com/square/okhttp/issues/1934
+             */
+
+            TLSSocketFactory tlsSocketFactory;
+
+            try {
+                tlsSocketFactory = new TLSSocketFactory();
+                httpBuilder.sslSocketFactory(tlsSocketFactory, tlsSocketFactory.systemDefaultTrustManager());
+            } catch (KeyManagementException | NoSuchAlgorithmException e) {
+                    Log.d(TAG, "Failed to create Socket connection ");
+                    e.printStackTrace();
+                }
+
+
+            final OkHttpClient client = httpBuilder.certificatePinner(certificatePinner)
                     .addNetworkInterceptor(interceptor)
+                    .connectionSpecs(Collections.singletonList(connectionSpec.build()))
                     .build();
 
 
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(END_POINT)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .client(okHttpClient)
+                    .client(client)
                     .build();
             sService = retrofit.create(GitHubApi.class);
         }
---
+++ app/src/main/java/com/pollux/publickeypinning/network/service/TLSSocketFactory.java
@@ -0,0 +1,88 @@
+package com.pollux.publickeypinning.network.service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+public class TLSSocketFactory extends SSLSocketFactory {
+
+    private SSLSocketFactory internalSSLSocketFactory;
+
+    public TLSSocketFactory() throws KeyManagementException, NoSuchAlgorithmException {
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new TrustManager[] { systemDefaultTrustManager() }, null);
+        internalSSLSocketFactory = sslContext.getSocketFactory();
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return internalSSLSocketFactory.getDefaultCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return internalSSLSocketFactory.getSupportedCipherSuites();
+    }
+
+    @Override
+    public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(s, host, port, autoClose));
+    }
+
+    @Override
+    public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+    }
+
+    @Override
+    public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port, localHost, localPort));
+    }
+
+    @Override
+    public Socket createSocket(InetAddress host, int port) throws IOException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+    }
+
+    @Override
+    public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(address, port, localAddress, localPort));
+    }
+
+    private Socket enableTLSOnSocket(Socket socket) {
+        if(socket != null && (socket instanceof SSLSocket)) {
+            ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.1", "TLSv1.2"});
+        }
+        return socket;
+    }
+
+    public X509TrustManager systemDefaultTrustManager() {
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                throw new IllegalStateException("Unexpected default trust managers:"
+                        + Arrays.toString(trustManagers));
+            }
+            return (X509TrustManager) trustManagers[0];
+        } catch (GeneralSecurityException e) {
+            throw new AssertionError(); // The system has no TLS. Just give up.
+        }
+    }
+}
